6c125908c51459d3c92969403fa601f13601f4aa
henryy-el <phonghngle@gmail.com>
2024-03-24 01:15:18 +0700

    fix(Metadata): update site url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app/',
+  siteUrl: 'https://blog-beta-lyart.vercel.app/',
   siteRepo: 'https://github.com/henryy-el/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
